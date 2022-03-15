dcb159f89e7bcec90cf0e76baf7a0a518810b3fe
Maria José Dillarza Ramos <mariajosedillarza@gmail.com>
2022-03-14 20:38:54 -0600

    Actividad 6

---
+++ 06_Actividad6_Metodos_De_Arreglos/Actividad6.js
@@ -0,0 +1,19 @@
+//Ejercicio 1
+var animals = ['eagle','parrot','monkey','boar','lion'];
+var comingSoonAnimals = ['panter','dragon','turtle'];
+var concatenatedAnimals = animals.concat(comingSoonAnimals);
+console.log(concatenatedAnimals);
+
+//Ejercicio 2
+var array = [4,6,1,0,8,2];
+console.log(array.sort());
+
+//Ejercicio 3
+var animals = ['eagle','parrot','monkey','boar','lion'];
+animals.push('cow');
+console.log(animals);
+
+//Ejercicio 4
+var animals = ['eagle','parrot','monkey','boar','lion'];
+animals.shift();
+console.log(animals);
