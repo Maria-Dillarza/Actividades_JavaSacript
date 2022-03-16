223c91e25baae68749732ae2c7de1dd87f7b1c2c
Maria José Dillarza Ramos <mariajosedillarza@gmail.com>
2022-03-16 12:41:33 -0600

    Actividad 8 terminada

--- 08_Actividad8_Funciones/Actividad8.js
+++ 08_Actividad8_Funciones/Actividad8.js
@@ -1,9 +1,14 @@
+
 //Ejercicio1
-console.log('Ejercicio 1')
-
-
-
-
+console.log('Ejercicio 1') 
+function searchWord(sentence, word) {
+  if(sentence.includes(word)) 
+    return true;
+    else return false;
+}
+//Test
+const result1 = searchWord('Hola mundo', 'perro')
+console.log(result1);
 
 //Ejercicio2
 console.log('Ejercicio 2')
@@ -12,9 +17,9 @@ function sumaArray(numbers) {
   for(let i = 0; i < numbers.length; i++) {
     sum += numbers[i];
   }
-  return sum;
+  return 'The sum of all the numbers is: ' + sum;
 }
-
+//Test
 const result2 = sumaArray([1,2,3,4,10,11])
 console.log(result2);
 
@@ -25,11 +30,27 @@ function isPalindrome(word) {
   const reverseArray = array.reverse();
   const reverseString = reverseArray.join('');
   if(reverseString == word){
-    return word + ' is a palindrome word'
+    return true + ' ' + word + ' is a palindrome word'
   }
   else {
-    return word + ' is not a palindrome word'
+    return false + ' ' + word + ' is not a palindrome word'
   }
 }
-const result3 = isPalindrome('one');
+//Test
+const result3 = isPalindrome('oso');
 console.log(result3);
+
+//Ejercicio 4
+console.log('Ejercicio 4');
+function temperatureConverter(temperature, scale) {
+  if (scale == '°C') {
+    fahrenheit = (temperature * 1.8) + 32;
+    return temperature + scale + ' ' + 'is equivalent to ' + fahrenheit + '°F';
+  } else {
+    celsius = (temperature -32) / 1.8;
+    return temperature + scale + ' ' + 'is equivalent to ' + celsius + '°C'
+  }
+}
+//Test
+const result4 = temperatureConverter(80, '°F');
+console.log(result4);
